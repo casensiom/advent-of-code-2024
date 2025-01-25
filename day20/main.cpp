4c0e402bdc81c5e88db414fcd8efe3d6ba609ca9
Carlos Asensio (Thales DIS) <carlos.asensiomartinez@thalesgroup.com>
2025-01-25 23:01:43 +0100

    implement day20 solution

--- day20/main.cpp
+++ day20/main.cpp
@@ -15,6 +15,35 @@ using namespace cam::parser;
 using namespace cam::pathfinder;
 using namespace cam::math;
 
+//              2
+//             212
+//            21x12
+//             212
+//              2
+
+//
+//              x12345678901234567890
+//              12345678901234567890
+//              2345678901234567890
+//              345678901234567890
+//              45678901234567890
+//              5678901234567890
+//              678901234567890
+//              78901234567890
+//              8901234567890
+//              901234567890
+//              01234567890
+//              1234567890
+//              234567890
+//              34567890
+//              4567890
+//              567890
+//              67890
+//              7890
+//              890
+//              90
+//              0
+
 size_t
 get_map(const std::vector<std::vector<int>> &map, Vector2i pos) {
     const int    x      = pos.getX();
@@ -30,32 +59,37 @@ get_map(const std::vector<std::vector<int>> &map, Vector2i pos) {
 }
 
 size_t
-improve(const std::vector<std::vector<int>> &map, size_t x, size_t y, size_t d) {
-    std::vector<Vector2i> dirs = {
-        { 0, -1},
-        { 1,  0},
-        { 0,  1},
-        {-1,  0}
-    };
+improve(const std::vector<std::vector<int>> &map, size_t x, size_t y, size_t len, size_t limit) {
+    const size_t HEIGHT = map.size();
+    const size_t WIDTH  = map[0].size();
 
-    Vector2i dir = dirs[d];
     Vector2i pos(x, y);
+    size_t   ret     = 0;
+    int      current = get_map(map, pos);
+    int      ilen    = (int)len;
+    for(int y = -ilen; y <= ilen; y++) {
+        for(int x = -ilen; x <= ilen; x++) {
+            int jump = abs(x) + abs(y);
+            if(jump > ilen) {
+                continue;
+            }
+            Vector2i npos = pos + Vector2i(x, y);
+            int      next = get_map(map, npos);
+            int      diff = (next - current) - jump;
+            if(next == 0 || next < current || diff <= 0) {
+                continue;
+            }
 
-    int current = get_map(map, pos);
-    int wall    = get_map(map, pos + dir);
-    int next    = get_map(map, pos + dir * 2);
-
-    size_t ret = 0;
-    if(current != 0 && wall == 0 && next != 0 && next > current) {
-        ret = (next - current) - 2;
+            if(diff >= limit) {
+                ret++;
+            }
+        }
     }
-
     return ret;
 }
 
 size_t
-solve(const std::vector<std::string> &lines, size_t limit) {
-    size_t     ret = 0;
+solve(const std::vector<std::string> &lines, size_t len, size_t limit) {
     PathFinder pf;
 
     const size_t HEIGHT = lines.size();
@@ -63,7 +97,7 @@ solve(const std::vector<std::string> &lines, size_t limit) {
 
     pf.set(lines);
     auto solv = pf.solve();
-    pf.dump();
+    // pf.dump();
 
     std::vector<std::vector<int>> map(HEIGHT, std::vector<int>(WIDTH, 0));
     for(size_t i = 0; i < solv.size(); i++) {
@@ -72,16 +106,11 @@ solve(const std::vector<std::string> &lines, size_t limit) {
         map[y][x] = i + 1;
     }
 
+    size_t ret = 0;
     for(size_t i = 0; i < solv.size(); i++) {
         int x = solv[i].getX();
         int y = solv[i].getY();
-        for(size_t d = 0; d < 4; d++) {
-            size_t diff = improve(map, x, y, d);
-            if(diff >= limit) {
-                std::cout << "break: (" << x << ", " << y << ") [" << d << "] = " << diff << std::endl;
-                ret++;
-            }
-        }
+        ret += improve(map, x, y, len, limit);
     }
 
     return ret;
@@ -89,12 +118,12 @@ solve(const std::vector<std::string> &lines, size_t limit) {
 
 size_t
 part1(const std::vector<std::string> &lines) {
-    return solve(lines, 100);
+    return solve(lines, 2, 100);
 }
 
 size_t
 part2(const std::vector<std::string> &lines) {
-    return 0;
+    return solve(lines, 20, 100);
 }
 
 int
